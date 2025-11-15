6bcb699ddf38903021021dd567b0da82dab229b7
vinivinoo <antoni.vince92@gmail.com>
2025-11-15 01:19:46 +0100

    backup

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -5,8 +5,9 @@
     settings = {
       mainBar = {
         position = "top";
-        modules-left = ["hyprland/workspaces"];
-        modules-center = [];
+
+        modules-left = [ "hyprland/workspaces" ];
+        modules-center = [ ];
         modules-right = [
           "tray"
           "bluetooth"
@@ -15,107 +16,84 @@
           "battery"
           "clock"
         ];
-      };
-        
-      "clock" = {
-        format = "{:%H:%M}";
-        format-alt = "{:%a %d %H:%M}";
-        locale = "de_DE.UTF-8";
-        tooltip-format = "<tt><small>{calendar}</small></tt>";
-        calendar = {
-          mode = "month";
-          weeks-pos = "right";
-          on-scroll = 1;
-          format = {
-            months = "<span color='#89b4fa'><b>{}</b></span>";
-            today = "<span color='#a6e3a1'><b>{}</b></span>";
-            weeks = "<span color='#b4befe'><b>{}</b></span>";
+      
+
+        clock = {
+          format = "{:%H:%M}";
+          format-alt = "{:%a %d %H:%M}";
+          locale = "de_DE.UTF-8";
+          tooltip-format = "<tt><small>{calendar}</small></tt>";
+          calendar = {
+            mode = "month";
+            weeks-pos = "right";
+            on-scroll = 1;
+            format = {
+              months = "<span color='#89b4fa'><b>{}</b></span>";
+              today = "<span color='#a6e3a1'><b>{}</b></span>";
+              weeks = "<span color='#b4befe'><b>{}</b></span>";
+            };
           };
         };
-      };
 
-      "battery" = {
-        states = {
-          warning = 30;
-          critical = 15;
+        battery = {
+          states = {
+            warning = 30;
+            critical = 15;
+          };
+          format = "{icon}";
+          format-warning = "{icon}";
+          format-critical = "{icon}";
+          format-charging = "";
+          format-plugged = "";
+          format-full = "";
+          format-icons = [ "" "" "" "" "" ];
+          tooltip-format = "{capacity}%";
         };
-        format = "{icon}";
-        format-warning = "{icon}";
-        format-critical = "{icon}";
-        format-charging = "";
-        format-plugged = "";
-        format-full = "";
-        format-icons = [ "" "" "" "" "" ];
-        tooltip-format = "{capacity}%";
-      };
 
-      "wireplumber" = {
-        format = "{icon}";
-        format-muted = "";
-        tooltip-format = "{volume}%";
-        format-icons = {
-          default = [ "" "" " " ];
+        wireplumber = {
+          format = "{icon}";
+          format-muted = "";
+          tooltip-format = "{volume}%";
+          format-icons.default = [
+            ""
+            ""
+            ""
+          ];
+          on-click = "pavucontrol";
         };
-        on-click = "pavucontrol";
-      };
 
-      "bluetooth" = {
-        format = "";
-        format-connected = "";
-        tooltip-format-connected = "{device_alias}";
-      };
+        bluetooth = {
+          format = "";
+          format-connected = "";
+          tooltip-format-connected = "{device_alias}";
+        };
 
-      "network" = {
-        format-wifi = "{icon}";
-        format-ethernet = "󰲝";
-        format-disconnected = "";
-        tooltip-format-wifi = "{essid}";
-        tooltip-format-ethernet = "{ifname}";
-        tooltip-format-disconnected = "Disconnected";
-        format-icons = [
-          "󰤯"
-          "󰤟"
-          "󰤢"
-          "󰤥"
-          "󰤨"
-        ];
-      };
+        network = {
+          format-wifi = "{icon}";
+          format-ethernet = "󰲝";
+          format-disconnected = "";
+          tooltip-format-wifi = "{essid}";
+          tooltip-format-ethernet = "{ifname}";
+          tooltip-format-disconnected = "Disconnected";
+          format-icons = [
+            "󰤯"
+            "󰤟"
+            "󰤢"
+            "󰤥"
+            "󰤨"
+          ];
+        };
 
-      "tray" = {
-        spacing = 15;
-        icons = { };
+        tray = {
+          spacing = 15;
+          icons = { };
+        };
       };
     };
     style = ''
-      @define-color rosewater #f5e0dc;
-      @define-color flamingo #f2cdcd;
-      @define-color pink #f5c2e7;
-      @define-color mauve #cba6f7;
-      @define-color red #f38ba8;
-      @define-color maroon #eba0ac;
-      @define-color peach #fab387;
-      @define-color yellow #f9e2af;
-      @define-color green #a6e3a1;
-      @define-color teal #94e2d5;
-      @define-color sky #89dceb;
-      @define-color sapphire #74c7ec;
-      @define-color blue #89b4fa;
-      @define-color lavender #b4befe;
-      @define-color text #cdd6f4;
-      @define-color subtext1 #bac2de;
-      @define-color subtext0 #a6adc8;
-      @define-color overlay2 #9399b2;
-      @define-color overlay1 #7f849c;
-      @define-color overlay0 #6c7086;
-      @define-color surface2 #585b70;
-      @define-color surface1 #45475a;
-      @define-color surface0 #313244;
-      @define-color base #1e1e2e;
-      @define-color mantle #181825;
-      @define-color crust #11111b;
 
       * {
-        font-family: "";
+        font-family: "FiraCode Nerd Font Mono";
         font-size: 16px;
         font-weight: bold;
       }
