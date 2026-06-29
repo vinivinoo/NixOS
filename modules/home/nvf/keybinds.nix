f0a88679e215ec566ee341cb1bdddcf0605b48e0
vinivinoo <antoni.vince92@gmail.com>
2026-06-30 01:40:55 +0200

    added new keybinds

--- modules/home/nvf/keybinds.nix
+++ modules/home/nvf/keybinds.nix
@@ -1,10 +1,48 @@
 {
-  programs.nvf.settings.vim.keymap = [
+  programs.nvf.settings.vim.keymaps = [
+    {
+      mode = [
+        "n"
+        "v"
+      ];
+      key = "x";
+      action = "\"_x";
+      silent = true;
+      desc = "Delete";
+    }
+    {
+      mode = "x";
+      key = "<leader>p";
+      action = "\"_dP";
+      silent = true;
+    }
     {
       mode = "n";
-      key = "x";
-      action = "_x";
+      key = "<leader>bb";
+      action = "\"_dP";
       silent = true;
+      desc = "Switch Buffer";
+    }
+    {
+      mode = "n";
+      key = "<leader>bi";
+      action = "<cmd>BufferDelete!<cr>";
+      silent = true;
+      desc = "Delete Invisible Buffers";
+    }
+    {
+      mode = "n";
+      key = "<leader>bd";
+      action = "<cmd>Bdelete<cr>";
+      silent = true;
+      desc = "Delete Buffers";
+    }
+    {
+      mode = "n";
+      key = "<leader>bx";
+      action = "<cmd>%bd<cr>";
+      silent = true;
+      desc = "Close Buffers";
     }
   ];
 }
