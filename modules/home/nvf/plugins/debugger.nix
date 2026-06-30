f2e07b1ed63323dc0ccd5759a8dfd11ca533a675
vinivinoo <antoni.vince92@gmail.com>
2026-06-30 02:35:46 +0200

    removed comment

--- modules/home/nvf/plugins/debugger.nix
+++ modules/home/nvf/plugins/debugger.nix
@@ -3,14 +3,5 @@
     debugger = {
       nvim-dap.enable = true;
     };
-    # keymaps = [
-    #   {
-    #     mode = "n";
-    #     key = "<leader>db";
-    #     action = "<cmd>lua require('dap').toggle_breakpoint()<cr>";
-    #     silent = true;
-    #     desc = "Toggle Breakpoint";
-    #   }
-    # ];
   };
 }
