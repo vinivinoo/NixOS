30fe91443188510d953c86a95f687be5ec871c02
vinivinoo <antoni.vince92@gmail.com>
2026-06-30 12:42:55 +0200

    added session manager

--- modules/home/nvf/plugins/session.nix
+++ modules/home/nvf/plugins/session.nix
@@ -2,8 +2,22 @@
   programs.nvf.settings.vim = {
     session = {
       nvim-session-manager = {
-        enable = false;
+        enable = true;
+        mappings = {
+          deleteSession = "<leader>qd";
+          loadLastSession = "<leader>ql";
+          loadSession = "<leader>qs";
+          saveCurrentSession = "<leader>qq";
+        };
+        setupOpts = {
+          autoload_mode = "Disabled";
+        };
       };
     };
+    # keymaps = [
+    #   {
+    #
+    #   }
+    # ]
   };
 }
