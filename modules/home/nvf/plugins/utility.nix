9fe2b45d9c51736c2c94e375f53abb4a54ed2c07
vinivinoo <antoni.vince92@gmail.com>
2026-06-30 12:43:44 +0200

    added image support to nvim
    added image-nvim and img-clip

--- modules/home/nvf/plugins/utility.nix
+++ modules/home/nvf/plugins/utility.nix
@@ -12,6 +12,10 @@
       nix-develop.enable = true;
       smart-splits.enable = true;
       yazi-nvim.enable = true;
+      images = {
+        image-nvim.enable = true;
+        img-clip.enable = true;
+      };
     };
     keymaps = [
       {
@@ -22,6 +26,18 @@
         key = "<leader>sr";
         action = "<cmd>lua local grug = require('grug-far'); local ext = vim.bo.buftype == '' and vim.fn.expand('%:e'); grug.open({ transient = true, prefills = { filesFilter = (ext and ext ~= '') and ('*.' .. ext) or nil } })<cr>";
         desc = "Search and Replace";
+      }
+      {
+        mode = "n";
+        key = "<leader>-";
+        action = "<cmd>Yazi<cr>";
+        desc = "File Manager Yazi";
+      }
+      {
+        mode = "n";
+        key = "<leader>cw";
+        action = "<cmd>Yazi cwd<cr>";
+        desc = "File Manager in Working Directory";
       }
     ];
   };
