a819f96d05df811bec0c49eb95c0499558a2b52a
vinivinoo <antoni.vince92@gmail.com>
2026-06-30 12:43:18 +0200

    changed tabline keybinds

--- modules/home/nvf/plugins/tabline.nix
+++ modules/home/nvf/plugins/tabline.nix
@@ -6,9 +6,9 @@
         mappings = {
           cycleNext = "<Tab>"; # Next buffer
           cyclePrevious = "<S-Tab>"; # Previous buffer
-          moveNext = "<leader>>"; # Move buffer right
-          movePrevious = "<leader><"; # Move buffer left
-          closeCurrent = "<leader>x"; # Close current buffer
+          moveNext = "<leader>b]"; # Move buffer right
+          movePrevious = "<leader>b["; # Move buffer left
+          closeCurrent = "<leader>bx"; # Close current buffer
           pick = "<leader>bp"; # Pick buffer interactively
           sortByDirectory = "<leader>bsd"; # Sort by directory
           sortByExtension = "<leader>bse"; # Sort by extension
@@ -23,6 +23,7 @@
             always_show_bufferline = true;
             show_buffer_close_icons = true;
             color_icons = true;
+            numbers = "ordinal";
             diagnostics = "nvim_lsp";
             indicator = {
               style = "icon";
